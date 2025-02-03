3e6620f9b824d468178a63d05a5797c09d6e2ce1
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-03 13:13:22 +0530

    Add react-scroll dependency; update Navbar for smooth scrolling navigation

--- ResponsiveDesign/EduCity/src/components/navbar/Navbar.tsx
+++ ResponsiveDesign/EduCity/src/components/navbar/Navbar.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
 
 export const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -13,13 +21,37 @@ export const Navbar = () => {
     <nav className={`container ${sticky ? "dark-background" : ""}`}>
       <img className="logo" src={logo} alt="" />
       <ul>
-        <li>Home</li>
-        <li>Programe</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className="btn">Contact us</button>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="programes" smooth={true} offset={0} duration={500}>
+            Programe
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={0} duration={500}>
+            About us
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="campus" smooth={true} offset={0} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="testimonials" smooth={true} offset={0} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} offset={0} duration={500}>
+            <button className="btn">Contact us</button>
+          </Link>
         </li>
       </ul>
     </nav>
